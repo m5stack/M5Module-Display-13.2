8ffd4453924888eccdba0e3b3057c106b5d38816
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-02-26 16:59:07 +0900

    Sample code is now compatible with M5Unified v0.1.4

--- M5Module-Display-13.2-Demo-Core2/M5Module-Display-13.2-Demo-Core2.ino
+++ M5Module-Display-13.2-Demo-Core2/M5Module-Display-13.2-Demo-Core2.ino
@@ -1,105 +1,79 @@
 #include <Arduino.h>
-#include "M5ModuleDisplay.h"
-#include "M5Unified.h"
 
-#include "wav_unsigned_8bit_click.cpp"
+#include <M5ModuleDisplay.h>
 
-static void play_wav(void*);
+// Version 0.1.4 or higher required
+#include <M5Unified.h>
 
+#include "sunrise_anna_single_left.h"
 extern const unsigned char pattern_test[13287];
 
-static M5ModuleDisplay display(1280, 720);
-static M5GFX lcd;
-
 void setup() {
-    auto cfg         = M5.config();
-    cfg.external_spk = true;
-    cfg.internal_spk = false;
+    auto cfg = M5.config();
+
+    cfg.external_speaker.module_display = true;
+    cfg.external_display.module_display = true;
+
+    // If you want to change the resolution, uncomment the following lines and
+    // adjust the values
+    //  cfg.module_display.logical_width = 1280;
+    //  cfg.module_display.logical_height = 720;
+    //  cfg.module_display.refresh_rate = 60;
 
     M5.begin(cfg);
 
-    display.begin();
-    display.setRotation(1);
-    display.setTextSize(1.5);
-    display.setFont(&fonts::efontCN_24_b);
-    display.startWrite();
-    display.drawPng(pattern_test, ~0u, 0, 0, display.width(), display.height(),
-                    0, 0, 3.0, 3.0, datum_t::middle_center);
-    display.endWrite();
+    M5.Display.setFont(&fonts::efontCN_24_b);
+    M5.Display.clear(WHITE);
+    M5.Display.setTextColor(ORANGE);
+    M5.Display.drawString("Display module 13.2 Demo", 20, 0);
+    M5.Display.setCursor(0, 80);
+    M5.Display.setTextColor(BLACK);
+    M5.Display.print("Loop Display:");
+    M5.Display.setCursor(0, 110);
+    M5.Display.print("Test Pic->Red->Green->Blue");
+    M5.Display.setTextColor(TFT_RED);
+    M5.Display.setCursor(20, 180);
+    M5.Display.print("A test wav will play!");
 
-    lcd.begin();
-    lcd.startWrite();
-    lcd.setFont(&fonts::efontCN_24_b);
-    lcd.clear(WHITE);
-    lcd.setTextColor(ORANGE);
-    lcd.drawString("Display module 13.2 Demo", 20, 0);
-    lcd.setCursor(0, 80);
-    lcd.setTextColor(BLACK);
-    lcd.print("Loop Display:");
-    lcd.setCursor(0, 110);
-    lcd.print("Test Pic->Red->Green->Blue");
-    lcd.setTextColor(TFT_RED);
-    lcd.setCursor(20, 180);
-    lcd.print("A test wav will play!");
-    lcd.endWrite();
+    // Change the primary display to ModuleDisplay.
+    M5.setPrimaryDisplayType(m5gfx::board_M5ModuleDisplay);
 
-    delay(2000);
+    M5.Display.setTextSize(1.5);
+    M5.Display.setFont(&fonts::efontCN_24_b);
 
-    auto spk_cfg        = M5.Speaker.config();
-    spk_cfg.sample_rate = 96000;
-    if (M5.getBoard() == m5gfx::boards::board_M5StackCore2) {
-        spk_cfg.pin_ws       = 0;
-        spk_cfg.pin_data_out = 2;
-        spk_cfg.pin_bck      = 27;
-    } else if (M5.getBoard() == m5gfx::boards::board_M5Stack) {
-        spk_cfg.pin_ws       = 0;
-        spk_cfg.pin_data_out = 15;
-        spk_cfg.pin_bck      = 12;
-    }
+    // If you want to change the volume, uncomment the following line and adjust
+    // the value M5.Speaker.setVolume(128);
 
-    M5.Speaker.config(spk_cfg);
-    M5.Speaker.begin();
-    M5.Speaker.setVolume(255);
-    M5.Speaker.setChannelVolume(0, 255);
-    M5.Speaker.setChannelVolume(1, 255);
-
-    xTaskCreatePinnedToCore(play_wav, "play_wav", 8192, NULL, 1, NULL, 1);
+    // Endless playback of wavedata.
+    M5.Speaker.playWav(sunrise_anna_single_left,
+                       sizeof(sunrise_anna_single_left), 0);
 }
 
 void loop(void) {
-    display.clear(TFT_RED);
-    display.drawCentreString("M5STACK Display Module 13.2", display.width() / 2,
-                             10);
-    display.drawCentreString("红(RED)", display.width() / 2,
-                             display.height() / 2);
+    M5.Display.clear(TFT_BLACK);
+    M5.Display.drawPng(pattern_test, ~0u, 0, 0, M5.Display.width(),
+                       M5.Display.height(), 0, 0, (M5.Display.width() / 320),
+                       (M5.Display.height() / 240), datum_t::middle_center);
     delay(4000);
 
-    display.clear(TFT_GREEN);
-    display.drawCentreString("M5STACK Display Module 13.2", display.width() / 2,
-                             10);
-    display.drawCentreString("绿(GREEN)", display.width() / 2,
-                             display.height() / 2);
+    M5.Display.clear(TFT_RED);
+    M5.Display.drawCentreString("M5STACK Display Module 13.2",
+                                M5.Display.width() / 2, 10);
+    M5.Display.drawCentreString("红(RED)", M5.Display.width() / 2,
+                                M5.Display.height() / 2);
     delay(4000);
 
-    display.clear(TFT_BLUE);
-    display.drawCentreString("M5STACK Display Module 13.2", display.width() / 2,
-                             10);
-    display.drawCentreString("蓝(BLUE)", display.width() / 2,
-                             display.height() / 2);
+    M5.Display.clear(TFT_GREEN);
+    M5.Display.drawCentreString("M5STACK Display Module 13.2",
+                                M5.Display.width() / 2, 10);
+    M5.Display.drawCentreString("绿(GREEN)", M5.Display.width() / 2,
+                                M5.Display.height() / 2);
     delay(4000);
 
-    display.clear(TFT_BLACK);
-    display.drawPng(pattern_test, ~0u, 0, 0, display.width(), display.height(),
-                    0, 0, (display.width() / 320), (display.height() / 240),
-                    datum_t::middle_center);
+    M5.Display.clear(TFT_BLUE);
+    M5.Display.drawCentreString("M5STACK Display Module 13.2",
+                                M5.Display.width() / 2, 10);
+    M5.Display.drawCentreString("蓝(BLUE)", M5.Display.width() / 2,
+                                M5.Display.height() / 2);
     delay(4000);
-}
-
-static void play_wav(void*) {
-    while (true) {
-        M5.Speaker.playWav(sunrise_anna_single_left,
-                           sizeof(sunrise_anna_single_left), ~0u, 0, true);
-        delay(3000);
-        M5.Speaker.stop(0);
-    }
 }
